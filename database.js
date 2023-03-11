91a8ac057917ade3d0d04dc0bacf4433eb01b2aa
Dylan <dylanw2@umbc.edu>
2023-03-11 18:01:53 -0500

    set up database and database functions. Create start of website with html and css

---
+++ database.js
@@ -0,0 +1,54 @@
+const sqlite3 = require('sqlite3').verbose();
+let sql;
+
+const db = new sqlite3.Database('./database.db', sqlite3.OPEN_READWRITE,(err)=>{
+    if (err){
+        return console.error(err.message);
+    }
+});
+
+// Create table
+function createTable(){
+    sql = 'CREATE TABLE users(first_name,last_name,id,points)'
+    db.run(sql);
+}
+
+// Remove Table
+function removeTable(){
+    sql = "DROP TABLE users"
+    db.run(sql)
+}
+
+
+// Insert Data into database
+function addUser(first_name,last_name,id,points){
+    sql = 'INSERT INTO users(first_name,last_name,id,points) VALUES (?,?,?,?)'
+    db.run(sql,[first_name,last_name,id,points],(err)=>{
+        if (err){
+            return console.error(err.message);
+        }  
+    })
+}
+
+// Delete User
+function deleteUser(id){
+    sql = 'DELETE FROM users WHERE id=?';
+    db.run(sql,[id],(err)=>{
+        if (err){
+            return console.error(err.message);
+        }
+    })
+}
+
+// Query the data
+function queryData(){
+    sql = 'SELECT * FROM users';
+    db.all(sql,[],(err,rows) => {
+        if (err){
+            return console.error(err.message);
+        }
+        rows.forEach((row) => {
+            console.log(row);
+        })
+    })
+}
